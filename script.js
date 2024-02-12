ee3910aa4d0c0c14a111a28c17bedc359f430415
Sam Kamali <97678867+Sam-Kamali@users.noreply.github.com>
2024-02-11 22:42:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const noBtn = document.querySelector(".no-btn");
 const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Good cuz I like </br> chocolate strawberries :P";
+  question.innerHTML = "Good!</br> you could be my chocolate :P";
 });
 noBtn.addEventListener("mouseover", () => {
   const i =
